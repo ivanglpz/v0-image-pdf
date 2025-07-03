aa3d6b03261ee4856582ce25012582731e3c5207
ivanglpz <miltonivan117@gmail.com>
2025-07-02 21:18:00 -0600

    update dimension images

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,8 +28,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: "/cover.png",
-        width: 1200,
-        height: 630,
+        width: 1024,
+        height: 1024,
         alt: "imgs-pdf cover image",
       },
     ],
